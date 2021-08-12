4f92f5042ef47b4eacda07297d69e4c15784a8e7
Charlotte <charlottecboyer@gmail.com>
2021-08-12 19:00:18 -0400

    fixed namespacing

--- scripts.js
+++ scripts.js
@@ -22,55 +22,57 @@ window.addEventListener("scroll", () => {
     }
 })
 
+//PORFOLIO CAROUSEL CODE 
+
+    let slideIndex = 1;
+
+    const prevButton = document.querySelector(".prev");
+
+    const nextButton = document.querySelector(".next");
+
+    nextButton.addEventListener('click', () => {
+        plusSlides(1)
+    });
+
+    prevButton.addEventListener('click', () => {
+        plusSlides(-1)
+    });
+
+    const dots = document.getElementsByClassName("dot");
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].addEventListener('click', function () {
+            currentSlide(parseInt(this.dataset.whichdot))
+        })
+    }
+
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        let i;
+        const slides = document.getElementsByClassName("app");
+
+        if (n > slides.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = slides.length }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none"
+        }
+
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+    }
 
 }
 
 myApp.init()
 
-let slideIndex = 1;
-
-const prevButton = document.querySelector(".prev");
-
-const nextButton = document.querySelector(".next");
-
-nextButton.addEventListener('click', () => {
-    plusSlides(1)
-});
-
-prevButton.addEventListener('click', () => {
-    plusSlides(-1)
-});
-
-const dots = document.getElementsByClassName("dot");
-    for (let i = 0; i < dots.length; i++) {
-        dots[i].addEventListener('click', function(){
-            currentSlide(parseInt(this.dataset.whichdot))
-        })
-    }
-
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    const slides = document.getElementsByClassName("app");
-
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"
-    }
-
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
