63c607e99948e90fcd6ccc5bb68cb9d739e07ea7
Charlotte <charlottecboyer@gmail.com>
2021-08-12 17:58:27 -0400

    js fixes and mediaqs

--- scripts.js
+++ scripts.js
@@ -1,10 +1,20 @@
-// const myApp = {}
+const myApp = {}
 
-// myApp.init = () => {
+myApp.init = () => {
+// hamburger menu @ 420 px 
 
+myApp.menu = document.querySelector("ul")
+
+myApp.menuButton = document.querySelector("button"); 
+
+myApp.menuButton.addEventListener('click', function(){
+    myApp.menu.classList.toggle('show')
+})
+
+
+//Scroll nav bar code 
 window.addEventListener("scroll", () => {
     const currentScroll = window.pageYOffset;
-    console.log(currentScroll)
     if (currentScroll > 615) {
         document.querySelector('nav').classList.add('navFixed');
     } else if (currentScroll < 615) {
@@ -12,7 +22,31 @@ window.addEventListener("scroll", () => {
     }
 })
 
-let slideIndex = 1; 
+
+}
+
+myApp.init()
+
+let slideIndex = 1;
+
+const prevButton = document.querySelector(".prev");
+
+const nextButton = document.querySelector(".next");
+
+nextButton.addEventListener('click', () => {
+    plusSlides(1)
+});
+
+prevButton.addEventListener('click', () => {
+    plusSlides(-1)
+});
+
+const dots = document.getElementsByClassName("dot");
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].addEventListener('click', function(){
+            currentSlide(parseInt(this.dataset.whichdot))
+        })
+    }
 
 showSlides(slideIndex);
 
@@ -23,23 +57,20 @@ function plusSlides(n) {
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
-function showSlides(n) {
-    let i; 
-    const slides = document.getElementsByClassName("app");
-    const dots = document.getElementsByClassName("dot");
 
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++){
+function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("app");
+
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"
     }
-    for (i = 0; i < dots.length; i++){
+
+    for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
-
-// }
-
-// myApp.init()
